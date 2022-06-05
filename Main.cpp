292fe9a7db4b70b68888c52196845af9720771ac
HyperBitGore <bruiserjoe01@gmail.com>
2022-06-05 12:40:35 -0500

    0.2
    All basic text editor features in, just missing so more quality of life things like copy and paste.

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,8 @@
 #define ESCAPE 27
 #define ENTER 10
 
+enum {WRITE = 5, ERASE, ISNEWLINE};
+
 bool exitf = false;
 
 //https://pdcurses.org/docs/MANUAL.html
@@ -20,19 +22,15 @@ int main() {
 	//halfdelay(1);
 	keypad(stdscr, TRUE);
 	start_color();
-	//init_pair()
 	size_t x = f1.data[f1.data.size() - 1].size() - 1;
 	size_t line = f1.data.size() - 1;
 	init_pair(1, COLOR_WHITE, COLOR_BLACK);
 	init_pair(2, COLOR_BLACK, COLOR_WHITE);
-	//add enter
 	//add copy and paste
 	//maybe add ability to multiple files open
 	//maybe add ctrl + s to save
 	while (!exitf) {
-		bool write = false;
-		bool er = false;
-		bool nl = false;
+		uint8_t change = 0;
 		int key = getch();
 		switch (key) {
 		case KEY_LEFT:
@@ -48,20 +46,17 @@ int main() {
 			line++;
 			break;
 		case BACKSPACE:
-			er = true;
-			write = false;
+			change = ERASE;
 			break;
 		case ENTER:
-			nl = true;
-			er = false;
-			write = false;
+			change = ISNEWLINE;
 			break;
 		case ESCAPE:
 			exitf = true;
 			break;
 		default:
 			if (key != ERR && key >= 32) {
-				write = true;
+				change = WRITE;
 			}
 			break;
 		}
@@ -77,6 +72,38 @@ int main() {
 		else if (x > f1.data[line].size() - 1) {
 			x = f1.data[line].size() - 1;
 		}
+		switch (change) {
+		case WRITE:
+			f1.data[line].insert(f1.data[line].begin() + x, key);
+			x++;
+			break;
+		case ERASE:
+			f1.data[line].erase(f1.data[line].begin() + x);
+			if (f1.data[line].size() == 0) {
+				f1.data.erase(f1.data.begin() + line);
+				line--;
+			}
+			else {
+				x--;
+			}
+			break;
+		case ISNEWLINE:
+			std::string temp;
+			int befn = 0;
+			for (int i = x; i < f1.data[line].size(); i++) {
+				if (f1.data[line][i] == '\n' && i != 0) {
+					befn = i;
+				}
+				temp.push_back(f1.data[line][i]);
+			}
+			if (temp.size() > 1) {
+				f1.data[line].erase(f1.data[line].begin() + x, f1.data[line].begin() + befn);
+			}
+			f1.data.insert(f1.data.begin() + line + 1, temp);
+			line++;
+			break;
+		}
+		
 		clear();
 		printw("FILE NAME : ");
 		printw(f1.name.c_str());
@@ -91,21 +118,13 @@ int main() {
 			for (auto& j : i) {
 				if (curline - 1 == line && cx == x) {
 					attron(COLOR_PAIR(2));
-					mvaddch(curline, cx, j);
+					if (j != '\n') {
+						mvaddch(curline, cx, j);
+					}
+					else {
+						mvaddch(curline, cx, ' ');
+					}
 					attroff(COLOR_PAIR(2));
-					if (write) {
-						i.insert(i.begin() + x + 1, key);
-						x++;
-						write = false;
-					}
-					else if (er) {
-						i.erase(i.begin() + x);
-						x--;
-						er = false;
-					}
-					else if (nl) {
-						
-					}
 				}
 				else {
 					attron(COLOR_PAIR(1));
